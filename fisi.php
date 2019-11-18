3a5c7044939532d9a122dc953e9808041bced6e7
Aluno <beatriztinobrito@hotmail.com>
2019-11-18 09:28:48 -0200

    eu

---
+++ fisi.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <!-- i=1,5A
+    r=0,25m
+    mi0=4pi.10a-7T.m/A -->
+<!-- 
+    a) B=10a-6T
+    b) B=0,6.10a-6T
+    c) B=1,2.10a-6T
+    d) B=2,4.10a-6T
+    e) B=2,4.10a-6T -->
+  </body>
+</html>
